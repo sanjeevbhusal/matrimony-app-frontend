2b71dbb3a6ff0517735dccd4010c3f9c06a09345
sanjeev <bhusalsanjeev23@gmail.com>
2023-11-21 12:01:33 +0545

    update navlinks

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,11 +10,16 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useAuth } from "@/hooks/useAuth";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const navLinks = [
+  {
+    label: "Home",
+    route: "/home",
+  },
   {
     label: "All Profiles",
     route: "/all-profiles",
@@ -28,16 +33,19 @@ const navLinks = [
 function NavBar() {
   const { user, logout } = useAuth();
   const router = useRouter();
+  const pathname = usePathname();
 
   return (
     <div className="absolute top-0 left-0 right-0 px-4 py-2 md:px-10 lg:px-16 flex justify-between items-center border-b">
-      <Image
-        src={Logo}
-        height={200}
-        width={200}
-        className="w-32 xl:w-48"
-        alt="Logo"
-      />
+      <Link href="/home">
+        <Image
+          src={Logo}
+          height={200}
+          width={200}
+          className="w-32 xl:w-48"
+          alt="Logo"
+        />
+      </Link>
       {!user ? (
         <div className="flex gap-3">
           <Link href="/login">
@@ -56,7 +64,16 @@ function NavBar() {
           <div className="flex gap-2">
             {navLinks.map((link) => (
               <Link href={link.route} key={link.label}>
-                <Button variant="ghost">{link.label}</Button>
+                <Button
+                  variant="ghost"
+                  className={cn({
+                    "text-blue-500 hover:text-blue-600": pathname.includes(
+                      link.route
+                    ),
+                  })}
+                >
+                  {link.label}
+                </Button>
               </Link>
             ))}
           </div>

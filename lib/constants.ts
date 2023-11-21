a26d9cbce7ade331c2b8775ebef2ffd79fb06ea8
sanjeev <bhusalsanjeev23@gmail.com>
2023-11-21 11:00:11 +0545

    update API_URL to make it work with mac

--- lib/constants.ts
+++ lib/constants.ts
@@ -8,5 +8,6 @@
 //   API_URL = "https://nest-react-production.up.railway.app";
 // }
 
-const API_URL = "http://localhost:3001";
+// const API_URL = "0.0.0.0:3001";
+const API_URL = "http://127.0.0.1:3001";
 export { API_URL };

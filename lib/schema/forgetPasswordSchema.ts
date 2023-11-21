34851bcc24eaf8aa95ee84dbb0164810d8b4f40d
sanjeev <bhusalsanjeev23@gmail.com>
2023-11-21 11:20:27 +0545

    change schema

--- lib/schema/forgetPasswordSchema.ts
+++ lib/schema/forgetPasswordSchema.ts
@@ -1,3 +1,7 @@
-import z from "zod";
+import { z } from "zod";
 
-export const ForgotPasswordSchema = z.object({});
+export const forgotPasswordSchema = z.object({
+  email: z.string().trim().email("Please enter a valid email address"),
+});
+
+export type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
